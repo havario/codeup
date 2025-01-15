4c25a1d5dd00f10266016bfaefe52137f6bfdd67
honeok <yihaohey@gmail.com>
2025-01-16 00:19:39 +0800

    update

---
+++ chapter2/escaptechar/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt" // fmt包主要提供格式化，输出、输入的函数
+
+func main()  {
+	// 转义字符的使用
+	fmt.Println("tom\tjack")
+
+	fmt.Println("hello\nworld")
+
+	fmt.Println("D:\\go_code\\codeup")
+
+	fmt.Println("ranbot say \"i love you\"")
+
+	// \r 回车
+	fmt.Println("天龙八部雪山飞狐\r张飞")
+}
