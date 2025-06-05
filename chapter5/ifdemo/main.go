2ba70d608567c35f6ee65e82123ad7670ff098d8
havario <honeokwork@gmail.com>
2025-06-05 11:21:25 +0800

    process control

--- chapter5/ifdemo/main.go
+++ chapter5/ifdemo/main.go
@@ -9,11 +9,20 @@ import (
 func main() {
 	// 编写一个程序, 可以输入人的年龄, 如果该同志的年龄大于18岁, 则输入 "你的年龄大于18岁, 要对自己的行为负责"
 
-	var age byte
-	fmt.Println("请输入年龄: ")
-	fmt.Scanln(&age)
+	// var age byte
+	// fmt.Println("请输入年龄: ")
+	// fmt.Scanln(&age)
 
-	if age > 18 {
+	// if age > 18 {
+	// 	fmt.Println("你的年龄大于18岁, 要对自己的行为负责!")
+	// }
+
+	// var age byte
+	// fmt.Println("请输入年龄: ")
+	// fmt.Scanln(&age)
+
+	// golang支持在if中直接定义一个变量
+	if age := 20; age > 18 {
 		fmt.Println("你的年龄大于18岁, 要对自己的行为负责!")
 	}
 }
