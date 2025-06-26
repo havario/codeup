cddd9fa4af1d032a8b4d1d842417f84703739d24
havario <157877551+havario@users.noreply.github.com>
2025-06-26 12:03:26 +0800

    continue
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ chapter5/exec/continueexec/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// continue实现 打印1~100之内的奇数 要求使用for循环+continue
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Printf("奇数是: %v\n", i)
+	}
+
+	// 从键盘读入个数不确定的整数, 并判断读入的正数和负数的个数, 输入为0时结束程序 (使用for循环, break, continue完成)
+	// positive  negative
+	var positiveCount int = 0 // 统计正数个数
+	var negativeCount int = 0 // 统计负数个数
+	var num int
+	for {
+		fmt.Printf("请输入一个整数: ")
+		fmt.Scanln(&num)
+		if num == 0 {
+			break
+		}
+		if num > 0 {
+			positiveCount++
+			continue
+		}
+		negativeCount++
+	}
+	fmt.Printf("正数个数是: %v 负数的个数是: %v\n", positiveCount, negativeCount)
+
+	// 某人有100000元, 每经过一次路口需要交费, 规则如下
+	// 当现金 > 50000元时, 每次交5%
+	// 当现金 < 50000元时, 每次交1000
+	// 计算该人可以经过多少次路口, 使用 for break 完成
+}
