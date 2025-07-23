c119f16a0174da0403804a8028d038c69ddf7720
havario <157877551+havario@users.noreply.github.com>
2025-07-23 15:49:58 +0800

    init func
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ chapter6/func6-init/main/main.go
@@ -0,0 +1,28 @@
+// Copyright 2025 (c) honeok <i@honeok.com>
+
+package main
+
+import (
+	"fmt"
+
+	U "github.com/havario/codeup/chapter6/func6-init/utils"
+)
+
+var age = pre()
+
+// 为了看到全局变量是先被初始化的, 这里先写函数完成初始化
+func pre() int {
+	fmt.Println("pre()")
+	return 90
+}
+
+// init函数, 通常可以在init函数中完成初始化工作
+func init() {
+	fmt.Println("init()")
+}
+
+// 如果一个文件同时包含全局变量定义, init函数和main函数, 则执行的流程是 变量定义 -> init函数 -> main函数
+func main() {
+	fmt.Println("main(), age = ", age)
+	fmt.Println("Age=", U.Age, "Name=", U.Name)
+}
