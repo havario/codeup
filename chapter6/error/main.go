7116222fd060b3f65f268aac2fb8f21beb8dbd7d
havario <157877551+havario@users.noreply.github.com>
2025-08-07 15:35:28 +0800

    自定义错误
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- chapter6/error/main.go
+++ chapter6/error/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -21,9 +22,33 @@ func test() {
 	fmt.Printf("result=%v\n", result) // panic: runtime error: integer divide by zero
 }
 
+// 读取一个配置文件config.ini的信息
+// 如果文件名传入不正确抛出自定义错误
+func readConfig(fileName string) (err error) {
+	if fileName == "config.ini" {
+		return nil
+	} else {
+		// 返回自定义错误
+		return errors.New("读取文件错误!")
+	}
+}
+
+func test02() {
+	err := readConfig("config.ini")
+	if err != nil {
+		// 如果读取文件错误发送错误, 就输出这个错误并终止程序
+		panic(err)
+	}
+
+	fmt.Printf("test02后续代码继续执行\n")
+}
+
 func main() {
 
 	// 测试go的错误
-	test()
+	// test()
+
+	// 测试自定义错误的使用
+	test02()
 	fmt.Printf("main函数代码\n")
 }
