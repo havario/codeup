5c1e38d2cf4b580ea8699dd816d82befbcb4fd62
havario <157877551+havario@users.noreply.github.com>
2025-08-13 21:35:30 +0800

    basic use of slice
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ chapter7/slicedemo01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 演示切片的基本使用
+	var intArr [5]int = [...]int{1, 22, 33, 66, 99}
+
+	/*
+		声明/定义一个切片
+		1> slice 就是切片的名称
+		2> intArr[1:3]表示slice引用到intArr这个数组
+		3> 引用intArr数组的起始下标为1, 最后的下标为3(不包含3)
+	*/
+	slice := intArr[1:3]
+	fmt.Println("intArr: ", intArr)
+	fmt.Println("slice元素是: ", slice)
+	fmt.Println("slice元素个数: ", len(slice))
+	fmt.Println("slice容量: ", cap(slice)) // slice目前可以存储的最大元素的个数, 容量可动态变化
+}
