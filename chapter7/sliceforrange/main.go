7d462e8ade71f32d8a60b2cd9eee63b0abc17179
havario <157877551+havario@users.noreply.github.com>
2025-08-19 18:20:23 +0800

    slice traversal
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ chapter7/sliceforrange/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// 使用常规for循环遍历切片
+	var arr [5]int = [...]int{10, 20, 30, 40, 50}
+	slice := arr[1:4] // 20 30 40
+	for i := 0; i < len(slice); i++ {
+		fmt.Printf("slice[%v]=%v \n", i, slice[i])
+	}
+
+	fmt.Println()
+
+	// 使用for range遍历切片
+	for i, v := range slice {
+		fmt.Printf("i=%v v=%v\n", i, v)
+	}
+}
