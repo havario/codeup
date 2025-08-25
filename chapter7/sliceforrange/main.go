027f7d0211b52cfb54761350adabf09f47af59a0
havario <157877551+havario@users.noreply.github.com>
2025-08-25 15:53:57 +0800

    notes on slicing
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- chapter7/sliceforrange/main.go
+++ chapter7/sliceforrange/main.go
@@ -10,15 +10,26 @@ func main() {
 
 	// 使用常规for循环遍历切片
 	var arr [5]int = [...]int{10, 20, 30, 40, 50}
-	slice := arr[1:4] // 20 30 40
+	// slice := arr[1:4] // 20 30 40
+	slice := arr[:] // 20 30 40 (0-结尾)
 	for i := 0; i < len(slice); i++ {
 		fmt.Printf("slice[%v]=%v \n", i, slice[i])
 	}
 
 	fmt.Println()
 
-	// 使用for range遍历切片
+	// 使用for--range遍历切片
 	for i, v := range slice {
 		fmt.Printf("i=%v v=%v\n", i, v)
 	}
+
+	fmt.Println()
+
+	/*
+		切片可以继续切片
+		slice已经是一个切片
+	*/
+	slice2 := slice[2:4] // 30 40
+	slice2[0] = 100      // 同理slice[0]也会变化, 因为slice和slice2指向同一个数据空间, 因此slice2变化slice也会变化
+	fmt.Println("slice2=", slice2)
 }
