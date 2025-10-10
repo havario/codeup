09afa6b41975d0037feecb9baafc9a4d1143fe9d
havario <157877551+havario@users.noreply.github.com>
2025-10-10 17:33:20 +0800

    初尝struct
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- chapter10/structdemo01/main.go
+++ chapter10/structdemo01/main.go
@@ -17,6 +17,30 @@ import (
 如果用户输入的小猫名错误, 则显示张老太没有这只猫猫。
 */
 
+/*
+定义cat结构体, 将cat的字段放入结构体
+在go里结构体struct是值类型
+*/
+
+type Cat struct {
+	Name  string
+	Age   int
+	Color string
+	Hobby string
+}
+
+/*
+结构体是自定义的数据类型, 代表一类事物
+结构体变量 (实例) 是具体的、实际的, 代表一个具体变量
+*/
+
 func main() {
-	fmt.Printf("random\n")
+	// 创建Cat变量
+	var cat1 Cat
+	cat1.Name = "小白"
+	cat1.Age = 3
+	cat1.Color = "白色"
+	cat1.Hobby = "fish"
+	fmt.Printf("cat1地址为: %p\n", &cat1)
+	fmt.Printf("cat1 = %v\n", cat1)
 }
